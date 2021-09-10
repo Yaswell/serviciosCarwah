4b66990ac19ae240550a2849d693f5dd44153a8b
Yaswell <48866195+Yaswell@users.noreply.github.com>
2021-09-10 12:28:02 -0400

    comint on master

---
+++ windows/Maed/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Maed::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Maed::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
---
+++ windows/Maed/ReactPackageProvider.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "winrt/Microsoft.ReactNative.h"
+
+namespace winrt::Maed::implementation
+{
+    struct ReactPackageProvider : winrt::implements<ReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider>
+    {
+    public: // IReactPackageProvider
+        void CreatePackage(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder) noexcept;
+    };
+} // namespace winrt::Maed::implementation
+
