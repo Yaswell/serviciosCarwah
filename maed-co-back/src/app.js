d996d0203ba194edc2f1306618a31ca75da94573
franklenny258 <franklenny258@gmail.com>
2021-10-02 22:45:33 -0400

    app modified

--- maed-co-back/src/app.js
+++ maed-co-back/src/app.js
@@ -4,18 +4,13 @@ const clientsRouter = require('./routers/clients');
 const inside_servicesRouter = require ('./routers/inside-services');
 const outside_servicesRouter = require('./routers/outside-services');
 const plansRouter = require('./routers/plans');
+const usersRouter = require('./routers/users');
 
 app.use(express.json());
 app.use(clientsRouter);
 app.use(inside_servicesRouter)
 app.use(outside_servicesRouter)
 app.use(plansRouter);
-
-
-const usersRouter = require('./routers/users');
-
-app.use(express.json());
-app.use(clientsRouter);
 app.use(usersRouter);
 
 
