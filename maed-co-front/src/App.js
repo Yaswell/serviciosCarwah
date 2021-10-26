d539cb65c3d8f6a8f53c5a317c59cd9e4b98c38b
Yaswell <20142223@itla.edu.do>
2021-10-26 16:05:04 -0400

    added import statement

--- maed-co-front/src/App.js
+++ maed-co-front/src/App.js
@@ -1,11 +1,11 @@
-
+import React, { Component }  from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Hey man</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
