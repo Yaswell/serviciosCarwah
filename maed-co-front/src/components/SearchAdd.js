1448f3aac8d14aa3f77f0744088d84468f89ff3f
NIcholas Mendez <nicholasmeeendez@gmail.com>
2021-12-05 00:23:37 -0400

    Animacion del boton agregar usuario
    Solo falta definir su estilo y hacerlo general.

--- maed-co-front/src/components/SearchAdd.js
+++ maed-co-front/src/components/SearchAdd.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import btnBuscar from '../assets/img/buscar.png'
-import IconoNuevo from '../assets/img/btnAgregar.png'
+import IconoNuevo from '../assets/img/add-user.png'
 
 const SearchAdd = ({value, onChange}) => {
     return (
         <div className="search-add">
             <input type="text" placeholder="Telefono del cliente" value= {value} onChange={onChange} />
             <button className="btnSearch" ><img src={btnBuscar} alt="buscar"/> </button>
-            <button className="btnNew"> <img src ={IconoNuevo} alt="" /> Agregar </button>
+            <button className="btnNew" data-hover="Agregar"> <div><img src ={IconoNuevo} alt="" /></div> </button>
         </div>
     )
 }
